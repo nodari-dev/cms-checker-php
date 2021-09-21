dcd7ad17b3a8a7e78809eb06a242543e5cae30e2
nodari-dev <nodari.pylypyshak@gmail.com>
2021-09-21 14:42:28 +0200

    migration

--- index.php
+++ index.php
@@ -1,274 +1,88 @@
 <?php
 
-$urls = [
-    'ilovecarousel.com',
-// 'mountainbikesdirect.com.au',
-// 'nopolka.com',
-// '',
-// 'comstockheritage.com',
-// 'coliesail.com',
-// 'propercloth.com',
-// 'picturesplus.com',
-// 'tileliving.com',
-// 'awe-tuning.com',
-// 'fitdir.com',
-// 'bkkgemstones.com',
-
-
+$CHECKER = [
+    "shopify" => ["_shopify_ga", "cdn.shopify", "myshopify"],
+    "wordpress" => ["wp-includes", "wp-content"],
+    "magento 1" => ["Mage.Cookies.path", "/skin/frontend/"],
+    "magento 2" => ["mage/cookies", "/static/", "/pub/static/", "static/version"]
 ];
 
-$check_list=[
-    "wp-includes",//WORDPRESS
-
-    'Mage.Cookies.path', //Cookies magento 1
-    '/skin/frontend/',//Magento 1
-
-    'mage/cookies', //Cookies magento 2
-    '/static/', //Magetno 2
-    '/pub/static/',//Magetno 2
-    'static/version',//Magento 2
-
-    '_shopify_ga',//Cookie shopify
-    'cdn.shopify',//Shopify
-
-
+$PERCENTAGE = [
+    "no cms found" => 1,
+    "shopify" => 0,
+    "wordpress" => 0,
+    "magento 1" => 0,
+    "magento 2" => 0
 ];
 
-//ADD HTTP OR HTTPS IF DOESNT EXIST
-function http_https($urls)
-{
-    $i = 0;
-    $el_check_id = 0;
+function openFile($file){
+    /**
+     * Opens file and starts
+     *
+     * @param string $file opens and set every new line as website
+     */
 
-    while ($i !== sizeof($urls)) {
-        $ch = curl_init('https://' . $urls[$el_check_id]);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'HEAD'); //its a  HEAD
-        curl_setopt($ch, CURLOPT_NOBODY, true);          // no body
-
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);  // in case of redirects
-        curl_setopt($ch, CURLOPT_VERBOSE, 0); //turn on if debugging
-        curl_setopt($ch, CURLOPT_HEADER, 1);     //head only wanted
-
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);    // we dont want to wait forever
-
-        curl_exec($ch);
-
-        $header = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-        if (strpos($urls[$el_check_id], 'https') !== false || strpos($urls[$el_check_id], 'http') !== false) {
-            $el_check_id++;
-        } elseif ($header == 0) {//no ssl
-            $urls[$el_check_id] = 'http://' . $urls[$el_check_id];
-            $el_check_id++;
-        } else {
-            $urls[$el_check_id] = 'https://' . $urls[$el_check_id];
-            $el_check_id++;
+    $handle = fopen($file, "r");
+    if ($handle) {
+        while (($websiteLink = fgets($handle)) !== false) {
+            checkContent(getWebsite($websiteLink));
         }
-        $i++;
-
+        fclose($handle);
+    } else {
+        echo "error opening the file." . "\n";
     }
 }
 
-function get_web($urls){
-    global $websites;
-$websites = [];
+function getWebsite($link) : array{
+    /**
+     * Curling website and checking 301 redirect if happened
+     *
+     * @param string $link as a full website link
+     *
+     * @return string $html as a full page document
+     */
 
-function get_web($urls, $websites){
-    $urls_download = 0;
-    $a = 0;
-    for ($i=0; $i < sizeof($urls); $i++){
-        $websites[$a] = file_get_contents($urls[$urls_download]);
-        $a++;
-        $urls_download++;
-    }
+    echo "getting website html..." . "\n";
+    $ch = curl_init($link);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,0);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    $html = curl_exec($ch);
+    curl_close($ch);
+    return array("html" => $html, "link" => $link);
 }
-//ADD HTTP OR HTTPS IF DOESN'T EXIST
 
+function checkContent($content){
+    /**
+     * Finding keywords from $content
+     * We use associated array $checker for checking
+     * And every found result will be +1 to $percentage[$cms]
+     *
+     * @param string $link as a full website link
+     */
 
-//CHECK ALL WEBSITES
+    $checker = $GLOBALS['CHECKER'];
+    $percentage = $GLOBALS['PERCENTAGE'];
 
-//First we need to check cookies on home pages
-//If can't, we search for special folder links
-//For some servers we need to use ini_set, because we get 403 (no permissions)
-function check_web($urls, $check_list, &$result)
-{
-    $i=1;
-    $web_el_id = 0;
-    $check_el_id = 0;
-
-    while ($i <= sizeof($urls)) {
-        if (strpos(file_get_contents($urls[$web_el_id] . '/wp-login.php'), $check_list[0])) {
-
-            $result[$web_el_id] = array($web_el_id, $urls[$web_el_id], 'wordpress');
-
-            echo $urls[$web_el_id] . ' wordpress' . '<br>';
-
-            $web_el_id++;
-        } elseif (strpos(file_get_contents($urls[$web_el_id]), $check_list[1])
-            || strpos(file_get_contents($urls[$web_el_id]), $check_list[2])) {
-
-            $result[$web_el_id] = array($web_el_id, $urls[$web_el_id], 'magento 1');
-
-
-            echo $urls[$web_el_id] . ' magento 1' . '<br>';
-
-            $web_el_id++;
-        } elseif (strpos(file_get_contents($urls[$web_el_id]), $check_list[3])
-            || strpos(file_get_contents($urls[$web_el_id]), $check_list[4])
-            || strpos(file_get_contents($urls[$web_el_id]), $check_list[5])
-            || strpos(file_get_contents($urls[$web_el_id]), $check_list[6])) {
-
-            $result[$web_el_id] = array($web_el_id, $urls[$web_el_id], 'magento 2');
-            echo $urls[$web_el_id] . ' magento 2';
-
-            $web_el_id++;
-        } elseif (strpos(file_get_contents($urls[$web_el_id]), $check_list[7])
-            || strpos(file_get_contents($urls[$web_el_id]), $check_list[8])) {
-
-            $result[$web_el_id] = array($web_el_id, $urls[$web_el_id], 'Shopify');
-            echo $urls[$web_el_id] . ' Shopify' . '<br>';
-            $web_el_id++;
-        } elseif (strpos(get_headers($urls[$web_el_id])[0], '403')) {
-            try {
-                ini_set('user_agent', 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3');
-                if (strpos(file_get_contents($urls[$web_el_id]), $check_list[7])
-                    || strpos(file_get_contents($urls[$web_el_id]), $check_list[8])) {
-                    throw new Exception($urls[$web_el_id] . ' Shopify' . '<br>');
-                } else {
-                    throw new Exception($urls[$web_el_id] . " " . get_headers($urls[$web_el_id])[0]);
-                }
-            } catch (Exception $ex) {
-                $result[$web_el_id] = array($web_el_id, $urls[$web_el_id], 'Shopify');
-
-                echo $urls[$web_el_id] . ' Shopify' . '<br>';
-
-                $web_el_id++;
-            }
-        } else {
-            $a = 1;
-            while ($a <= sizeof($urls[$web_el_id])) {
-                $curlInit = curl_init($urls[$web_el_id]);
-                curl_setopt($curlInit, CURLOPT_CONNECTTIMEOUT, 10);
-                curl_setopt($curlInit, CURLOPT_HEADER, true);
-                curl_setopt($curlInit, CURLOPT_NOBODY, true);
-                curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
-
-                //get answer
-                $response = curl_exec($curlInit);
-                if ($response) {
-                    $result[$web_el_id] = array($web_el_id, $urls[$web_el_id], 'Fault');
-                    echo $urls[$web_el_id] . ' Fault' . '<br>';
-                    $web_el_id++;
-                } else {
-                    $result[$web_el_id] = array($web_el_id, $urls[$web_el_id], 'Dead');
-                    echo $urls[$web_el_id] . ' Dead' . '<br>';
-                    $web_el_id++;
-                }
-                curl_close($curlInit);
-                $a++;
+    foreach ($checker as $cms => $value) {
+        foreach ($value as $keyword){
+            if(strpos($content["html"], $keyword)){
+                $percentage[$cms] += substr_count($content["html"], $keyword);
             }
         }
-        $i++;
     }
+    generateResult($percentage, $content["link"]);
 }
 
-function check_web($websites, $check_list, $urls){
-    $result_web[0] = array('Number', 'Website', 'CMS');
-    $result_web_el_id = 1;
-    $el_web_id = 0;
-    $i = 1;
-    while ($i <= sizeof($websites)) {
-        if(strpos($websites[$el_web_id], $check_list[0])) {
-            // print_r($urls[$el_web_id]);
-            // echo ' CMS WORDPRESS<br>';
-            $cms = 'wodpress';
-            $result_web[$result_web_el_id] = array($el_web_id, $urls[$el_web_id], $cms);
-            $result_web_el_id++;
-            $el_web_id++;
-
-        } elseif(strpos($websites[$el_web_id], $check_list[1]) && strpos($websites[$el_web_id], $check_list[2])) {
-            // print_r($urls[$el_web_id]);
-            // echo ' CMS MAGENTO 1<br>';
-            $cms = 'magento 1';
-            $result_web[$result_web_el_id] = array($el_web_id, $urls[$el_web_id], $cms);
-            $result_web_el_id++;
-            $el_web_id++;
-        }
-        elseif(strpos($websites[$el_web_id], $check_list[3]) || strpos($websites[$el_web_id], $check_list[4])) {
-            // print_r($urls[$el_web_id]);
-            // echo 'CMS MAGENTO 2<br>';
-            $cms = 'magento 2';
-            $result_web[$result_web_el_id] = array($el_web_id, $urls[$el_web_id], $cms);
-            $result_web_el_id++;
-            $el_web_id++;
-        }
-        else{
-            // print_r($urls[$el_web_id]);
-            // echo ' CMS NO<br>';
-            $el_check_id++;
-            $cms = "undefinded";
-            $result_web[$result_web_el_id] = array($el_web_id, $urls[$el_web_id], $cms);
-            $result_web_el_id++;
-            $el_web_id++;
-        }
-    $i++;
-
-    }
-    print_r($result_web);
+function chooseCMS($percentage):string{
+    return array_search(max($percentage), $percentage);
 }
 
-get_web($urls);
-check_web($websites, $check_list, $urls);
-header('Content-Type: text/csv');
-header('Content-Disposition: attachment; filename="table.csv"');
-
-$fp = fopen('../table.csv', 'w'); 
-foreach ($result_web as $fields) {
-    fputcsv($fp, $fields, ',');
+function generateResult($percentage, $link){
+    echo $link . " " . chooseCMS($percentage)."\n";
+    //TODO: save result as csv file
 }
-//CHECK ALL WEBISTES
-
-
-//SAVE INTO FILE
-function save_result($result){
-    $counter = 1;
-    $file_name = './results/' .'result-'.date("D-M-j_").$counter.'.csv';
-    if (file_exists($file_name)) {
-        $counter++;
-        echo $relative_pathrelative_path;
-        $fp = fopen('./results/' .'result-'.date("D-M-j_").$counter.'.csv', 'w');
-
-
-    }
-    else {
-        echo $relative_pathrelative_path;
-        $fp = fopen('./results/' .'result-'.date("D-M-j_").$counter.'.csv', 'w');}
-            foreach ($result as $fields) {
-            fputcsv($fp, $fields);
-        }
-        fclose($fp);
-        echo '<a href="'.$file_name.'">Download result</a>';
-}
-//SAVE INTO FILE
-
-
-
-
-$result[0] = array('Number', 'Website', 'CMS');
-
-if (sizeof($urls) > 0){
-    http_https($urls);
-    check_web($urls, $check_list, $result);
-    save_result($result);
-} else{
-     echo "No websites.txt selected" . "\n";
-}
-
-fclose($fp);
-
-get_web($urls, $websites);
-check_web($websites, $check_list);
-?>
+openFile("websites.txt");
